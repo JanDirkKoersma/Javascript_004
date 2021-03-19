bb0b5873cd490215c05a837dd3567ae1cb88d060
JanDirkKoersma <75176681+JanDirkKoersma@users.noreply.github.com>
2021-03-19 08:29:26 +0100

    Add files via upload

---
+++ Javascript_004/script.js
@@ -0,0 +1,24 @@
+const myAge = 25;
+
+if (myAge >= 18 && myAge <= 25) {
+  console.log("Je krijgt 50% korting");
+} else {
+  console.log("Je krijgt geen korting");
+}
+
+const name = "Sarah";
+
+if (name === "Sarah" || name === "Bram") {
+  console.log("Je krijgt een gratis biertje");
+} else {
+  console.log("Je krijgt geen gratis biertje");
+}
+
+const totalAmount = 20;
+
+if (totalAmount > 25 ) {
+    console.log("je krijgt gratis bitterballen")
+} else {
+    console.log("je krijgt een gratis portie nachos")
+}
+
